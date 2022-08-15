7c1a0aa476a0cf80fe14855be1b6e51f3015b2da
Johnny <Jarvan1110@gmail.com>
2022-08-15 17:19:20 +0800

    mybatis-annotation done.

--- mybatis-annotation/README.md
+++ mybatis-annotation/README.md
@@ -24,6 +24,40 @@
 
 <br>
 
-使用 resultMap 
+其他都略吧，跟 xml 基本一樣，只是換一種配置方式而已
 
+<br>
+
+細節都寫在 __RoleMapper__ 裡面，有需要再去看。
+
+<br>
+
+## Provider 方式
+
+```java
+public class PrivilegeProvider {
+
+    public String selectById(final Long id){
+        return new SQL(){
+            {
+                SELECT("id, privilege_name, privilege_url");
+                FROM("sys_privilege");
+                WHERE("id = #{id}");
+            }
+        }.toString();
+    }
+}
+```
+
+<br>
+
+```java
+public interface PrivilegeMapper {
+
+    // method 方法返回的返回值必須是 String
+    @SelectProvider(type = PrivilegeProvider.class, method = "selectById")
+    SysPrivilege selectById(Long id);
+
+}
+```
 
--- mybatis-annotation/src/main/java/com/frizo/lab/mybatis/mapper/RoleMapper.java
+++ mybatis-annotation/src/main/java/com/frizo/lab/mybatis/mapper/RoleMapper.java
@@ -2,10 +2,7 @@ package com.frizo.lab.mybatis.mapper;
 
 
 import com.frizo.lab.mybatis.model.SysRole;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.ResultMap;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -35,5 +32,34 @@ public interface RoleMapper {
     @ResultMap("roleResultMap") // 借用上面的 Results Ｍybatis 3.3.1 以上版本才可以這樣用
     @Select("select * from sys_role")
     List<SysRole> selectAll();
+
+    @Insert({"insert into sys_role(id, role_name, enabled, create_by, create_time)",
+            "values(#{id}, #{roleName}, #{enabled}, #{createBy}, #{createTime, jdbcType=TIMESTAMP})"
+    })
+    int insert(SysRole role);
+
+    @Insert({"insert into sys_role(role_name, enabled, create_by, create_time)",
+            "values(#{roleName}, #{enabled}, #{createBy}, #{createTime, jdbcType=TIMESTAMP})"
+    })
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int insertReturnKey(SysRole role);
+
+    @Insert({"insert into sys_role(role_name, enabled, create_by, create_time)",
+            "values(#{roleName}, #{enabled}, #{createBy}, #{createTime, jdbcType=TIMESTAMP})"
+    })
+    @SelectKey(statement = "SELECT LAST_INSERT_ID()", keyProperty = "id", resultType = Long.class, before = false)
+    int insertReturnKey2(SysRole role);
+
+    @Update({"update sys_role",
+            "set role_name = #{roleName},",
+            "enabled = #{enabled},",
+            "create_by = #{createBy},",
+            "create_time = #{createTime, jdbcType=TIMESTAMP}",
+            "where id = #{id}"
+    })
+    int update(SysRole role);
+
+    @Delete("delete from sys_role where id = #{id}")
+    int deleteById(Long id);
 
 }
